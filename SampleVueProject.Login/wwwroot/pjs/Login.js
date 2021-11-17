4f8b5f995f7eb932492edee9423e73399f2e0ce1
ooz <ooz@ziraatteknoloji.com>
2021-11-17 13:59:45 +0300

    updated

--- SampleVueProject.Login/wwwroot/pjs/Login.js
+++ SampleVueProject.Login/wwwroot/pjs/Login.js
@@ -14,38 +14,7 @@
             console.log('Created..');
         },
         methods: {
-            login() {
-                this.isLoading=true;
-                console.log('Login metodu tetiklendi,api çağrılacak..');
-
-                axios.get('https://localhost:5001/weatherforecast')
-                        .then(response =>
-                        {
-                            
-                            console.log('Başarılı sonuçlandı.');
-                            console.log(response.data.isSuccess);
-                            console.log(response.data.requestId);
-                            console.log(response.data.data.token);
-                            console.log(response.data.data.name);
-                            console.log(response.data.data.surname);
-                            console.log(response.data.data.requestTime);
-                            console.log(response.data.data.responseTime);
-                            this.isSuccess = true;
-                  
-                        })
-                        .catch(error =>
-                        {
-                            console.log(error);
-                            this.isSuccess = false;
-                            this.errorMessage = error;
-                        });
-
-
-                console.log('Login metodu tetiklendi,api bitti.Error Message:' + this.errorMessage);
-                this.isLoading= false;
-
-          
-            },
+             
             async login2() {
                 this.isLoading = true;
                 console.log('Login2 metodu tetiklendi,api çağrılacak..');
